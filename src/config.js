2081f0438f7e0697cc4f5723109bdaebbee59118
leejh3224 <leejh3224@naver.com>
2017-10-25 21:29:33 +0900

    Readme.md에 주의 사항 추가

--- src/config.js
+++ src/config.js
@@ -31,6 +31,16 @@ const config = {
       port: 6379,
       db: 1,
     },
+    naver: {
+      clientID: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET,
+      callbackURL: '/user/naver/callback',
+    },
+    facebook: {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: '/user/facebook/callback',
+    },
   }
 }
 
